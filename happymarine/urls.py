168a83c467b0a416581ea8dd38d92f5e8091498d
Rishan3119 <Rishan3119@gmail.com>
2025-02-20 15:48:00 +0400

    first commit

--- happymarine/urls.py
+++ happymarine/urls.py
@@ -10,7 +10,7 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/HappyMarineShipping/',include("api.HappyMarineShipping.urls"))
-]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+]
 
 
 urlpatterns += staticfiles_urlpatterns()
